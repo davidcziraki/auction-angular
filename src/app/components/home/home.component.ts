f8e9cfdf872353945501bbb3840056d6a84b7989
David Cziraki <cziraki-d@ulster.ac.uk>
2025-02-22 03:21:30 +0000

    added homepage content

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,10 +5,19 @@ import { InputText } from 'primeng/inputtext';
 import { InputIcon } from 'primeng/inputicon';
 import { Ripple } from 'primeng/ripple';
 import { RouterLink } from '@angular/router';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule, IconField, InputText, InputIcon, Ripple, RouterLink],
+  imports: [
+    ButtonModule,
+    IconField,
+    InputText,
+    InputIcon,
+    Ripple,
+    RouterLink,
+    Card,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
